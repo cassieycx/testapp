a2ff9401e2eb6f37771e1a5dff9352f476670c4c
cy177 <cy177@duke.edu>
2022-02-04 16:52:46 -0500

    test webhook

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
 cd C:\Users\vcm\Desktop\Code\tesstapp
 git pull https://%TESTAPP_GIT_PAT%@github.com/cassieycx/testapp.git
-venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe -m pip install -r requirements.txt -q
 venv\Scripts\python.exe server.py
