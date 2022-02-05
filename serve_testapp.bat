5c2b4c35eaf45ee47a701ac6461438777ebde49c
cy177 <cy177@duke.edu>
2022-02-04 20:57:46 -0500

    test webhook 9

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
-%DATE% %TIME%
+echo %DATE% %TIME%
 cd C:\Users\cy177\Desktop\Code\tesstapp
 git pull https://%TESTAPP_GIT_PAT%@github.com/cassieycx/testapp.git
 venv\Scripts\python.exe -m pip install -r requirements.txt -q
